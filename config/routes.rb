538cd9c756780abe6716f6e73e36de3f862435d1
Robson Raphael <robsonraphael2001@gmail.com>
2022-10-24 15:50:05 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get '/basic/:level', to: 'attributes#basic'
+  get 'attribute/basic', to: 'attributes#basic'
 
-  get '/force/:level', to: 'attributes#force'
+  get 'attribute/basic/up', to: 'attributes#up_basic'
+
+  get 'attribute/force', to: 'attributes#force'
+
+  get 'equipament/basic', to: 'equipaments#basic'
+
+  get 'equipament/weapon', to: 'equipaments#weapon'
 end
